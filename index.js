2690c98342fed2e42dc399ad6f28818c91d9b7f9
tomposbea <tompos.beata@yahoo.com>
2018-11-07 16:45:07 +0100

    initial chatbot

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express=require('express')
+const bodyParser=require('body-parser')
+const request=require('request')
+
+const app=express()
+
+app.set('port',(process.env.PORT ||5000))
+
+app.use(bodyParser.urlencoded{extended:false})
+app.use(bodyParser.json())
+
+app.get('/',function(req,res){
+    res.send("Hello")
+})
+
+app.get('/webhook/',function(req,res){
+    if(req.query['hub.verify_token']==='1234'){
+        res.send(req.query['hub.challenge'])
+    }
+    res.send("Wrong token")
+})
+
+app.listen(app.get('port'),function(){
+    console.log("runnin: port")
+})
