b31479d656a8a7e5f49a7d88c09d0206a9818bdf
tomposbea <tompos.beata@yahoo.com>
2018-11-07 17:03:50 +0100

    javitott

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app=express()
 
 app.set('port',(process.env.PORT ||5000))
 
-app.use(bodyParser.urlencoded{extended:false})
+app.use(bodyParser.urlencoded{extended:false}))
 app.use(bodyParser.json())
 
 app.get('/',function(req,res){
